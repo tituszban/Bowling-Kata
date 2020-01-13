910e124c5edcb70f3ae780d4c3793907c4ceb282
Titusz Ban <titusz.ban@trayport.com>
2020-01-13 12:19:33 +0000

    Create bowling project framework

---
+++ Bowling/BowlingLine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bowling
+{
+    public static class BowlingLine
+    {
+        public static uint? SubmitRolls(uint[] pinsKnockedDown)
+        {
+            return null;
+        }
+    }
+}
---
+++ Bowling/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bowling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rolls = new uint[] {0, 0, 0, 0, 0, 0, 0, 0, 0};
+            var score = BowlingLine.SubmitRolls(rolls);
+            Console.WriteLine("Score: {0}", score.HasValue ? score.Value.ToString() : "Invalid");
+
+        }
+    }
+}
